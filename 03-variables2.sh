2a884fcbe7bf5578273c846480a07ecf968e4f67
shaheer0071 <shaikshaheer007@gmail.com>
2025-01-06 21:40:54 +0530

    devops

---
+++ 03-variables2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=RAMESH
+PERSON2=SURESH
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: hello $PERSON2"
+echo "$PERSON1:: how are you doing?"
+echo "$PERSON2:: iam good.How are you?"
