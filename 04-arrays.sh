fa27ec37359bc1a473930c1502e673ff19af8260
shaheer0071 <shaikshaheer007@gmail.com>
2025-01-06 21:57:07 +0530

    devops

--- 04-arrays.sh
+++ 04-arrays.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-MOVIES=("pushpa" "rrr" "devara")
+MOVIES=("tiger" "sahoo" "kalki")
 
 
 echo "First movie: ${MOVIES[0]}"
 echo "Second movie: ${MOVIES[1]}"
 echo "Third movie: ${MOVIES[2]}"
 
-echo "All movies are: ${MOVIES[@]}"
+
